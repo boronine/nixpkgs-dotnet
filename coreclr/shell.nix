aaa32717986399f2b4d28b4a19fe185b9291652d
Roman Kuznetsov <roman@kuznero.com>
2017-10-18 17:54:36 +0200

    Introduce coreclr 2.0 package

---
+++ coreclr/shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? (import <nixpkgs> {}) }:
+
+(import ./default.nix) {
+  stdenv            = pkgs.stdenv;
+  fetchFromGitHub   = pkgs.fetchFromGitHub;
+  which             = pkgs.which;
+  cmake             = pkgs.cmake;
+  clang             = pkgs.clang;
+  llvmPackages      = pkgs.llvmPackages;
+  libunwind         = pkgs.libunwind;
+  gettext           = pkgs.gettext;
+  openssl           = pkgs.openssl;
+  python2           = pkgs.python2;
+  icu               = pkgs.icu;
+  lttng-ust         = pkgs.lttng-ust;
+  liburcu           = pkgs.liburcu;
+  libuuid           = pkgs.libuuid;
+  libkrb5           = pkgs.libkrb5;
+}
